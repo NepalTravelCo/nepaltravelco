1ca06b4bf4609118aa2fa5e41d716dea4a7269dd
Rojan Dhungel <rojandhungel11@gmail.com>
2025-08-01 17:05:31 +0545

    changed some styles

--- src/header-component/Navigation.tsx
+++ src/header-component/Navigation.tsx
@@ -281,7 +281,7 @@ const Navigation = () => {
                           </svg>
                         </a>
                         <a href="#" className="finland-menu-link" id="cultural-tours-link">
-                          <span>Cultural Tours</span>
+                          <span>Explore The Valley</span>
                           <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
                             <path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
                           </svg>
